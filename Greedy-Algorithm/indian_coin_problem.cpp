498e1e08f602ebf46ad483b94cc847e15e89166e
tarun <arunaggarwal096@gmail.com>
2021-10-26 16:53:21 +0530

    greedy approch

---
+++ Greedy-Algorithm/indian_coin_problem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int x;
+    cin >> x;
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans += x / a[i];
+        x -= x / a[i] * a[i];
+    }
+
+    cout << ans << endl;
+}
