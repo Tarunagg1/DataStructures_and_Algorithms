5b2555f5ea3ca557bba8067a21f46b962fadf7ca
--add <tarun@iotric.com>
2022-10-04 11:49:55 +0530

    single linked list

--- datastructure-revision/single_linked_list.c
+++ datastructure-revision/single_linked_list.c
@@ -37,6 +37,13 @@ struct Node *insertationFront(struct Node *head, int data)
     return newNode;
 }
 
+struct Node *deleteFront(struct Node *head)
+{
+    struct Node *temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
 
 struct Node *insertAtIndex(struct Node *head, int data, int index)
 {
@@ -55,6 +62,23 @@ struct Node *insertAtIndex(struct Node *head, int data, int index)
     return head;
 }
 
+struct Node *deleteAtIndex(struct Node *head, int index)
+{
+    struct Node *temp = head;
+    int i = 0;
+
+    while (i != index - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    struct Node *mac = temp->next;
+    temp->next = mac->next;
+    free(mac);
+
+    return head;
+}
 
 struct Node *insertationBack(struct Node *head, int data)
 {
@@ -70,16 +94,33 @@ struct Node *insertationBack(struct Node *head, int data)
     return head;
 }
 
+struct Node *deleteAtBack(struct Node *head)
+{
+    struct Node *p = head;
+    struct Node *q = head->next;
+
+    while (q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+
+    p->next = NULL;
+    free(q);
+    return head;
+}
 
 int main()
 {
     struct Node *head = createNewNode(1);
     head->next = createNewNode(2);
-    // head->next->next = createNewNode(3);
     head = insertationFront(head, 0);
 
     head = insertAtIndex(head, 6, 2);
     head = insertationBack(head, 3);
+    // head = deleteFront(head);
+    // head = deleteAtIndex(head,2);
+    head = deleteAtBack(head);
 
     traversal(head);
     return 0;
